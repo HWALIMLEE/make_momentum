a28e12fb93e6c511b5d16965354f46df05208f0a
HWALIMLEE <hwalim9612@gmail.com>
2022-02-06 22:57:29 +0900

    #7.1 adding todos

--- js/todo.js
+++ js/todo.js
@@ -3,11 +3,20 @@ const toDoInput = toDoForm.querySelector("input");
 // const toDoInput = document.querySelector("#todo-form input"); -> same
 const toDoList = document.querySelector("#todo-list");
 
+
+function paintToDo(newTodo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span"); //span은 li 하위에 있어야함
+    li.appendChild(span);
+    span.innerText = newTodo;
+    console.log(li);
+    toDoList.appendChild(li);
+}
 function handleToDoSubmit(event) {
     event.preventDefault(); //바로 새로고침 되지 않음
-    console.log(toDoInput.value);
     const newTodo = toDoInput.value;
     toDoInput.value = ''; //empty
+    paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
