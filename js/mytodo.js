e9188eb5d91c994e2949869294b50eb36692588e
HWALIMLEE <hwalim9612@gmail.com>
2022-02-19 23:52:12 +0900

    final challenge start

---
+++ js/mytodo.js
@@ -0,0 +1,5 @@
+const loginForm = document.querySelector("#login-form");
+const loginINput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
+const greeting = document.querySelector("#greeting");
+
