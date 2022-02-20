aa6f034ea74324e30ac3e88cd785352d85ec89e2
HWALIMLEE <hwalim9612@gmail.com>
2022-02-20 21:44:18 +0900

    delete

--- js/mytodo.js
+++
@@ -1,5 +0,0 @@
-const loginForm = document.querySelector("#login-form");
-const loginINput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
-const greeting = document.querySelector("#greeting");
-
